eed46f5b98661182bd2a8ca3d864a7b0c2d2f0c3
VíctorManuel <manuelluoficial@gmail.com>
2024-12-05 19:28:03 +0100

    buscaminas

--- Index.js
+++ Index.js
@@ -9,12 +9,12 @@ for (let i = 0; i < llista_numeros.length; i++) {
       console.log(llista_numeros[i]);
 }
 
-function mostraArray(){
+function mostraArray() {
       let div = document.getElementById("div_llista_numeros");
       llista_numeros.forEach(
-      function(numero){
-            div.innerHTML += "<div>"+numero+"</div>";
-      });
+            function (numero) {
+                  div.innerHTML += "<div>" + numero + "</div>";
+            });
 }
 mostraArray();
 
@@ -24,17 +24,17 @@ llista_bidimensional = new Array(
 );
 
 for (let i = 0; i < llista_bidimensional.length; i++) {
-      for (let k = 0; k < llista_bidimensional[i].length;k++){
+      for (let k = 0; k < llista_bidimensional[i].length; k++) {
             llista_bidimensional[i][k] = Math.floor(Math.random() * 10);
       }
 }
 
-function mostraArrayBidimensional(){
+function mostraArrayBidimensional() {
       let div = document.getElementById("div_llista_numeros");
       for (let i = 0; i < llista_bidimensional.length; i++) {
             div.innerHTML += "<div>"
-            for (let k = 0; k < llista_bidimensional[i].length;k++){            
-                  div.innerHTML += "<div class=\"item_array\">"+llista_bidimensional[i][k]+"</div>";
+            for (let k = 0; k < llista_bidimensional[i].length; k++) {
+                  div.innerHTML += "<div class=\"item_array\">" + llista_bidimensional[i][k] + "</div>";
             }
             div.innerHTML += "</div>"
       }
@@ -53,36 +53,36 @@ let div_torres = [
       document.getElementById("torre2"),
       document.getElementById("torre3")
 ];
-  
-let torres = [[],[],[]];
+
+let torres = [[], [], []];
 
 let colors = ["#fc333c", "#fc6a08", "#ffff0b", "#3f92ff", "#47fe3c"];
 
 function asignarNumber() {
       let numero = parseInt(document.getElementById("number").value);
-      
-      torres = [[],[],[]]
+
+      torres = [[], [], []]
       torre1.innerHTML = "";
-  
+
       for (let i = numero; i > 0; i--) {
-          torres[0].push(i);
+            torres[0].push(i);
       }
-      updateTowers() 
+      updateTowers()
 }
-  
-function moveDisk(from,to) {
+
+function moveDisk(from, to) {
       let toTower = torres[to - 1];
       let fromTower = torres[from - 1];
-  
-      if (fromTower.length == 0){
-          alert("No hay discos para mover");
-      }else {
-          let disk = fromTower[fromTower.length - 1];
-  
-          if (toTower.length == 0 || disk < toTower[toTower.length - 1]) {
-              toTower.push(fromTower.pop()); //Mover el disco, lo eliminamos de from y lo pasamos a la otra torre
-              updateTowers()
-          }
+
+      if (fromTower.length == 0) {
+            alert("No hay discos para mover");
+      } else {
+            let disk = fromTower[fromTower.length - 1];
+
+            if (toTower.length == 0 || disk < toTower[toTower.length - 1]) {
+                  toTower.push(fromTower.pop()); //Mover el disco, lo eliminamos de from y lo pasamos a la otra torre
+                  updateTowers()
+            }
       }
 }
 
@@ -92,16 +92,78 @@ function updateTowers() {
       let widthIncrement = 40; // Incremento por cada disco más grande
 
       for (let i = 0; i < torres.length; i++) {
-          div_torres[i].innerHTML = "";
+            div_torres[i].innerHTML = "";
 
-          for (let k = torres[i].length - 1; k >= 0; k--) {
-              let disco = torres[i][k];
-              let width = baseWidth + (disco - 1) * widthIncrement; // Calcula el ancho del disco
-              let color = colors[disco - 1]; // Asigna el color basado en el disco
+            for (let k = torres[i].length - 1; k >= 0; k--) {
+                  let disco = torres[i][k];
+                  let width = baseWidth + (disco - 1) * widthIncrement; // Calcula el ancho del disco
+                  let color = colors[disco - 1]; // Asigna el color basado en el disco
 
-              div_torres[i].innerHTML += `
+                  div_torres[i].innerHTML += `
                   <li style="width: ${width}px; background-color: ${color}">${disco}</li>
               `;
+            }
+      }
+}
+
+/* BUSCAMINAS --------------------------- */
+
+let tablero = new Array(8);
+/* */
+for (let i = 0; i < tablero.length; i++) {
+      tablero[i] = [];
+      for (let k = 0; k < tablero.length; k++) {
+            tablero[i][k] = 0;
+      }
+}
+
+function place_bomb() {
+      let bombs = 0;
+
+      while(bombs < 10){
+            let row = Math.floor(Math.random() * 8);
+            let colunm = Math.floor(Math.random() * 8);
+            if (tablero[row][colunm] === 0){
+                  let bombsPerRow = 0; 
+                  for (let i = 0; i < tablero[row].length; i++) {
+                        if (tablero[row][i] == 'x') {
+                              bombsPerRow++;
+                        }
+                  }
+                  if(bombsPerRow < 3) {
+                        tablero[row][colunm] = 'x';
+                        bombs++;
+                  }
+            }
+      }
+
+}
+
+function count_adjacent_bombs() {
+      for (let i = 0; i < tablero.length; i++) {
+          for (let k = 0; k < tablero[i].length; k++) {
+              if (tablero[i][k] == 'x') {
+                  continue; // Saltar las celdas con bombas
+              }
+              let countBombs = 0;
+  
+              // Verificar las celdas adyacentes
+              if (k > 0 && tablero[i][k - 1] == 'x') countBombs++; // Izquierda
+              if (k < tablero[i].length - 1 && tablero[i][k + 1] == 'x') countBombs++; // Derecha
+              if (i > 0 && tablero[i - 1][k] == 'x') countBombs++; // Arriba
+              if (i < tablero.length - 1 && tablero[i + 1][k] == 'x') countBombs++; // Abajo
+              if (i > 0 && k > 0 && tablero[i - 1][k - 1] == 'x') countBombs++; // Arriba izquierda
+              if (i > 0 && k < tablero[i].length - 1 && tablero[i - 1][k + 1] == 'x') countBombs++; // Arriba derecha
+              if (i < tablero.length - 1 && k > 0 && tablero[i + 1][k - 1] == 'x') countBombs++; // Abajo izquierda
+              if (i < tablero.length - 1 && k < tablero[i].length - 1 && tablero[i + 1][k + 1] == 'x') countBombs++; // Abajo derecha
+  
+              tablero[i][k] = countBombs; // Asignar el conteo de bombas adyacentes
           }
       }
-  }
+}
+
+console.log(tablero);
+console.log(place_bomb());
+console.log(count_adjacent_bombs());
+
+/*----------------------------------------*/
